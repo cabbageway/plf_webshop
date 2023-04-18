6e35fe60ee79b8d19b61b3bb3c1cffa41f28edd0
Gernot Loibner <lb@new.edvhtl.at>
2023-04-18 10:28:31 +0200

    preparation for students

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,39 @@
 import React, {ReactNode, useState} from 'react';
 import {productData} from './common/models/mockDataProducts';
-import {IProduct} from './common/models/IProduct';
-
-import Header from "./views/header/Header";
-import ProductList from "./views/product-list/ProductList";
-import Warenkorb from "./views/warenkorb/Warenkorb";
-import DetailsView from "./views/details-view/DetailsView";
-
 
 export enum AppViews {
   ProductOverview = 1,
   Warenkorb = 2,
   ProductDetails = 3,
-  Login = 4,
-  Meldung = 5
 }
 
 
 function App() {
 
   const [products, setproducts] = useState(productData);
-  const [warenkorb, setWarenkorb] = useState([{
-    PIndex: 0,
-    name: "",
-    category: "",
-    image: "",
-    price: 0.0,
-    anzahlImWarenkorb: 0
-  }]);
+  const [warenkorb, setWarenkorb] = useState([]);
 
-  const [detailP, setdetailP] = useState(productData[0]);
-
-  const inWarenkorb = (p: IProduct) => {
-
-    let existP = warenkorb.find((item) => {
-      return (item == p);
-    })
-
-    let warenkorbNew: IProduct[] = [];
-
-    if (existP == undefined) {
-      warenkorbNew = [...warenkorb, p];
-    } else {
-      existP.anzahlImWarenkorb++;
-      warenkorbNew = [...warenkorb];
-    }
-
-    setWarenkorb(warenkorbNew);
-  }
-
-  const minusWarenkorb = (p: IProduct) => {
-
-    let warenkorbNew = warenkorb.map(w => {
-      if (w.PIndex === p.PIndex) {
-        w.anzahlImWarenkorb--;
-        return w;
-      } else {
-        return w;
-      }
-    })
-
-    const warenkorbFiltered = warenkorbNew.filter(w => w.anzahlImWarenkorb > 0);
-
-    setWarenkorb(warenkorbFiltered);
-
-  }
   const [selectedView, setSelectedView] = useState(AppViews.ProductOverview);
-
-  const clickProducts = () => {
-    setSelectedView(AppViews.ProductOverview);
-  }
-
-  const clickWarenkorb = () => {
-    setSelectedView(AppViews.Warenkorb);
-  }
-
-  const clickLogin = () => {
-    setSelectedView(AppViews.Login);
-  }
-  const toDetails = (p: IProduct) => {
-    setdetailP(p);
-    setSelectedView(AppViews.ProductDetails);
-  }
-
 
   const renderSelectedView = (): ReactNode => {
     switch (selectedView) {
       case AppViews.ProductOverview:
         return (
-            <ProductList products={products} inWarenkorb={inWarenkorb} toDetails={toDetails}/>
+            <div>Replace with Product-List Component</div>
 
         )
       case AppViews.Warenkorb:
         return (
-            <Warenkorb warenkorb={warenkorb} inWarenkorb={inWarenkorb}
-                       minusWarenkorb={minusWarenkorb}/>
+            <div>Replace with Warenkorb Component</div>
         )
-      case AppViews.ProductDetails:
-        return (
-            <DetailsView detailedProduct={detailP} toShowProducts={clickProducts}/>
-        )
+
     }
   }
 
   return (
       <div className="App container">
 
-        <Header clickProducts={clickProducts} clickWarenkorb={clickWarenkorb}
-                clickLogin={clickLogin}/>
+        <div>Replace with Header Component</div>
         <div className="app-content">
           {renderSelectedView()}
         </div>
--- src/common/models/mockDataProducts.tsx
+++ src/common/models/mockDataProducts.tsx
@@ -1,12 +1,12 @@
 import {IProduct} from "./IProduct";
-
-
+import flattv from "../../assets/flat-tv.jpg"
+import iphone from "../../assets/iphone.jpg"
 export const productData: IProduct[] = [
     {
         PIndex : 1,
         name: "Iphone 12",
         category: "Electonic",
-        image:"https://files.refurbed.com/ii/iphone-12-1607327724.jpg?t=resize&h=600&w=800",
+        image:flattv,
         price: 950.00,
         anzahlImWarenkorb:1
     },
@@ -14,18 +14,8 @@ export const productData: IProduct[] = [
         PIndex : 2,
         name: "Flat TV",
         category: "Elektro",
-        image: "https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_92411809/fee_786_587_png",
+        image: iphone,
         price: 499.00,
         anzahlImWarenkorb: 1
     }
 ];
-
-// export const userData: user[] = [{
-//     username: "Chris",
-//     userpwd: "1234"
-// },
-//     {
-//         username: "Gernot",
-//         userpwd: "5678"
-//     }
-// ]
